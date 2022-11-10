e4a8985c414a438c1177c7493f21e1a358bb6d60
Marvelous Akporowho <solomonmarvel@hotmail.com>
2022-11-11 00:40:37 +0100

    added config options DEVELOPMENT & PRODUCTION

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,27 @@
-/**
- * The global configuration file is used to set configuration parameters
- * that would be used throughout the application
- */
- global.AppConfig = {
+require('dotenv').config()
+let development = {
     PORT: process.env.PORT || 3000,
     HOST: process.env.HOST || 'http://localhost:3000',
     DBURL: process.env.DBURL || 'mongodb://localhost:27017',
     JWTSECRET: process.env.JWTSECRET || 'topuniversesecretkey',
     MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
     SECRET: process.env.SECRET
-    
 }
+
+let production = {
+    PORT: 3000,
+    HOST: '',
+    DBURL: 'mongodb://localhost:27017',
+    JWTSECRET: '',
+    MAILGUN_API_KEY: '',
+    SECRET: ''
+}
+
+let config 
+if(!process.env.NODE_ENV) {
+    config = development
+} else {
+    config = (process.env.NODE_ENV === "development") ? development : production
+}
+
+global.AppConfig = config
