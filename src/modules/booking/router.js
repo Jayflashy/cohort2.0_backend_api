a8a22d3d8fbdac3f9ccfba3defbfa6ebe253e122
Joshua Adeyemi <109805121+Jayflashy@users.noreply.github.com>
2022-11-11 10:19:01 +0100

    Update router.js
    Remove auth from the route

--- src/modules/booking/router.js
+++ src/modules/booking/router.js
@@ -2,6 +2,6 @@ const bookingRouter = require('express').Router()
 const { createBooking } = require('../booking/controller')
 const {checkJWT } = require('../authorisation/middleware')
 
-bookingRouter.post('/auth/booking/:id',checkJWT, createBooking)
+bookingRouter.post('/booking/:id',checkJWT, createBooking)
 
 module.exports = bookingRouter
